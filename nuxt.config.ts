dcbb95a78d0c920c8ff00a95fda7fd5a611bb054
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2025-07-17 21:50:27 +0100

    Add Favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,13 @@ export default defineNuxtConfig({
     { path: '~/components/icons' },
   ],
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    },
+  },
   compatibilityDate: '2025-05-15',
   eslint: {
     config: {
